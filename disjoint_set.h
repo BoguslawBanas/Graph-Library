783b72f5b69958a81ca797c44d1d3ff458991c3c
BoguslawBanas <bogus.banas@interia.pl>
2025-06-12 23:39:16 +0200

    Changes in Disjoint_Set class.

--- disjoint_set.h
+++ disjoint_set.h
@@ -6,40 +6,56 @@
 
 class Disjoint_Set{
 private:
-    std::vector<int32_t>s;
+    std::vector<uint32_t>parent;
+    std::vector<uint32_t>rank;
+
 public:
-    Disjoint_Set(uint32_t value) {
-        s=std::vector<int32_t>(value, -1);
+    Disjoint_Set(uint32_t size){
+        parent=std::vector<uint32_t>(size);
+        rank=std::vector<uint32_t>(size, 0);
+        for(int32_t i=0;i<size;++i){
+            parent[i]=i;
+        }
     }
 
     ~Disjoint_Set()=default;
 
-    uint32_t find(const uint32_t vertex) const{
-        if(vertex>=s.size()){
+    uint32_t find(const uint32_t vertex){
+        if(vertex>=parent.size()){
             printErrorMsg(2, "Trying to reach an index outside of a size of a Disjoint_set.");
         }
-        int32_t tmp=vertex;
-        int32_t tmp2=s[vertex];
-        while(tmp2!=-1) {
-            tmp=tmp2;
-            tmp2=s[tmp];
+
+        if(parent[vertex]!=vertex){
+            parent[vertex]=find(parent[vertex]);
         }
-        return tmp;
+        return parent[vertex];
     }
 
     void unionize(const uint32_t v1, const uint32_t v2) {
-        if(v1>=s.size() || v2>=s.size()){
+        if(v1>=parent.size() || v2>=parent.size()){
             printErrorMsg(2, "Trying to reach an index outside of a size of a Disjoint_set.");
         }
+
         uint32_t fv1=find(v1);
         uint32_t fv2=find(v2);
-        if(fv1<fv2) s[fv2]=fv1;
-        else s[fv1]=fv2;
+        if(fv1==fv2){
+            return;
+        }
+
+        if(rank[fv1]<rank[fv2]){
+            parent[fv1]=fv2;
+        }
+        else if(rank[fv1]>rank[fv2]){
+            parent[fv2]=fv1;
+        }
+        else{
+            parent[fv2]=fv1;
+            ++rank[fv1];
+        }
     }
 
-    void unionize_f(const uint32_t fv1, const uint32_t fv2){
-        if(fv1<fv2) s[fv2]=fv1;
-        else s[fv1]=fv2;
+    inline bool areNodesConnected(const uint32_t v1, const uint32_t v2){
+        return find(v1)==find(v2);
     }
 };
 
